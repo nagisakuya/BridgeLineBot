d0f01323b7628f5c40099f426a73c9d053d906ea
nagisakuya <nagisakuya@gmail.com>
2023-02-23 22:52:35 +0900

    sqlにテーブル作ってメッセージを送るところまで

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,36 @@
 use std::net::SocketAddr;
-use std::{
-    fs,
-    path::PathBuf,
-};
+use std::{fs, path::PathBuf};
 //use serde_json::Value;
-use axum::*;
 use axum::body::Bytes;
+use axum::*;
 use axum_server::tls_rustls::*;
 use once_cell::sync::Lazy;
-use serde::Serialize;
+
+use chrono::{prelude::*, Duration};
 
 mod message;
 pub use message::*;
+use sqlx::Row;
 
 const TOKEN: Lazy<String> =
     Lazy::new(|| fs::read_to_string("token").expect("failed to read token file"));
-const TLS_KEY_DIR: Lazy<PathBuf> =
-    Lazy::new(|| PathBuf::from(&fs::read_to_string("TLS_KEY_DIR_PATH").expect("failed to read TLS_KEY_DIR_PATH file")));
+const TLS_KEY_DIR: Lazy<PathBuf> = Lazy::new(|| {
+    PathBuf::from(
+        &fs::read_to_string("TLS_KEY_DIR_PATH").expect("failed to read TLS_KEY_DIR_PATH file"),
+    )
+});
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let app = Router::new().route("/ping", routing::get(ping))
-    .route("/", routing::post(print_request));
+    //let sqlite = sqlx::sqlite::SqliteConnection::connect("sqlite::memory:").await?;
+
+    let app = Router::new()
+        .route("/ping", routing::get(ping))
+        .route("/", routing::post(print_request));
 
     let rustls_config = RustlsConfig::from_pem_file(
-        TLS_KEY_DIR.join("fullchain.pem"), 
-        TLS_KEY_DIR.join("privkey.pem")
+        TLS_KEY_DIR.join("fullchain.pem"),
+        TLS_KEY_DIR.join("privkey.pem"),
     )
     .await
     .unwrap();
@@ -50,47 +55,80 @@ async fn ping() -> &'static str {
     "Hello, World!"
 }
 
-async fn print_request(body:Bytes) -> &'static str {
-    println!("{}",String::from_utf8(body.to_vec()).unwrap());
+async fn print_request(body: Bytes) -> &'static str {
+    println!("{}", String::from_utf8(body.to_vec()).unwrap());
     "Ok"
 }
 
-#[derive(Serialize)]
-struct BloadcastMessage {
-    messages: Vec<Box<dyn Message>>,
-}
-impl BloadcastMessage {
-    async fn send(&self) {
-        let resp = send_request(
-            "https://api.line.me/v2/bot/message/broadcast",
-            &serde_json::to_string(self).unwrap(),
-        )
-        .await;
-        println!("{:?}", resp);
-    }
+async fn create_attendance_check(
+    sql_pool: sqlx::Pool<sqlx::Sqlite>,
+    time: DateTime<Local>,
+    duration: Duration,
+) {
+    //ランダムid生成
+    use rand::Rng;
+    let attendance_id = "attendance".to_owned() + &rand::thread_rng().gen::<u64>().to_string();
+
+    //sqlに登録
+    sqlx::query("insert into schedule(type,status,sending_schedule,finishing_schedule,data1) values(?,?,?,?,?)")
+    .bind("attendance")
+    .bind("unsended")
+    .bind(time - duration)
+    .bind(time)
+    .bind(&attendance_id)
+    .execute(&sql_pool).await.unwrap();
+
+    //出欠管理用のテーブル作成
+    sqlx::query(&format!(
+        "create table {attendance_id}(user_id string,status string)"
+    ))
+    .execute(&sql_pool)
+    .await
+    .unwrap();
+
+    //(とりあえず)メッセージ送信
+    let message = BloadcastMessage {
+        messages: vec![Box::new(FlexMessage::new(generate_flex(&attendance_id)))],
+    };
+
+    message.send().await;
 }
 
-async fn send_request(url: &str, body: &str) -> Result<reqwest::Response, reqwest::Error> {
-    let client = reqwest::Client::new();
-    client
-        .post(url)
-        .header("Content-Type", "application/json")
-        .bearer_auth(TOKEN.to_string())
-        .body(body.to_string())
-        .send()
-        .await
+fn generate_flex(id: &str) -> serde_json::Value {
+    let mut text = fs::read_to_string("vote_flex_message.json").unwrap();
+    text = text.replace("%ID%", id);
+    serde_json::from_str(&text).unwrap()
 }
 
 #[tokio::test]
 async fn test() {
-    let message = BloadcastMessage {
-        messages: vec![
-            //Box::new(SimpleMessage::new("あいうえお")),
-            //Box::new(SimpleMessage::new("かきくけこ")),
-            Box::new(FlexMessage::new(
-                serde_json::from_reader(fs::File::open("vote_flex_message.json").unwrap()).unwrap(),
-            )),
-        ],
-    };
-    message.send().await;
+    let pool = sqlx::SqlitePool::connect("database.sqlite").await.unwrap();
+    create_attendance_check(pool, Local::now(), Duration::seconds(100)).await;
+}
+
+#[tokio::test]
+async fn sqlite_test() {
+    let pool = sqlx::SqlitePool::connect("database.sqlite").await.unwrap();
+    let var = sqlx::query("select * from test")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+    for item in var {
+        let id: i32 = item.try_get("id").unwrap();
+        let text: String = item.try_get("text").unwrap();
+        let datatime: DateTime<Local> = item.try_get("datetime").unwrap();
+        println!("{},{},{}", id, text, datatime);
+    }
+}
+
+#[tokio::test]
+async fn sqlite_insert_test() {
+    let pool = sqlx::SqlitePool::connect("database.sqlite").await.unwrap();
+    let _ = sqlx::query("insert into test(id,text,datetime) values (?,?,?)")
+        .bind(123)
+        .bind("あいうえお")
+        .bind(Local::now())
+        .execute(&pool)
+        .await
+        .unwrap();
 }
--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,74 +1,113 @@
-use serde::Serialize;
+use super::*;
 use erased_serde::serialize_trait_object;
-use std::fs::File;
+use serde::Serialize;
 
-pub trait Message : erased_serde::Serialize + 'static + Send + Sync{
+
+#[derive(Serialize)]
+pub struct BloadcastMessage {
+    pub messages: Vec<Box<dyn Message>>,
+}
+impl BloadcastMessage {
+    pub async fn send(&self) {
+        let resp = send_request(
+            "https://api.line.me/v2/bot/message/broadcast",
+            &serde_json::to_string(self).unwrap(),
+        )
+        .await;
+        println!("{:?}", resp);
+    }
+}
+
+async fn send_request(url: &str, body: &str) -> Result<reqwest::Response, reqwest::Error> {
+    let client = reqwest::Client::new();
+    client
+        .post(url)
+        .header("Content-Type", "application/json")
+        .bearer_auth(TOKEN.to_string())
+        .body(body.to_string())
+        .send()
+        .await
+}
+
+#[tokio::test]
+async fn send_bloadcast_test() {
+    let message = BloadcastMessage {
+        messages: vec![
+            //Box::new(SimpleMessage::new("あいうえお")),
+            //Box::new(SimpleMessage::new("かきくけこ")),
+            Box::new(FlexMessage::new(
+                serde_json::from_reader(fs::File::open("vote_flex_message.json").unwrap()).unwrap(),
+            )),
+        ],
+    };
+    message.send().await;
+}
+
+pub trait Message: erased_serde::Serialize + 'static + Send + Sync {
     fn json(&self) -> String;
 }
 serialize_trait_object!(Message);
 
 #[derive(Serialize)]
-pub struct SimpleMessage{
+pub struct SimpleMessage {
     #[serde(rename = "type")]
-    type_:String,
-    pub text:String,
+    type_: String,
+    pub text: String,
 }
-impl SimpleMessage{
-    pub fn new(string:&str) -> Self{
-        SimpleMessage{
-            type_:"text".to_string(),
-            text:string.to_string()
+impl SimpleMessage {
+    pub fn new(string: &str) -> Self {
+        SimpleMessage {
+            type_: "text".to_string(),
+            text: string.to_string(),
         }
     }
 }
-impl Message for SimpleMessage{
-    fn json(&self) -> String{
+impl Message for SimpleMessage {
+    fn json(&self) -> String {
         serde_json::to_string(self).unwrap()
     }
 }
 
 #[derive(Serialize)]
 #[allow(non_snake_case)]
-pub struct FlexMessage{
+pub struct FlexMessage {
     #[serde(rename = "type")]
-    type_:String,
-    altText:String,
+    type_: String,
+    altText: String,
     #[serde(rename = "contents")]
-    pub json:serde_json::Value,
+    pub json: serde_json::Value,
 }
-impl FlexMessage{
-    pub fn new(json:serde_json::Value) -> Self{
-        FlexMessage{
-            type_:"flex".to_string(),
-            altText:"altText:flexMessageです".to_string(),
-            json:json
+impl FlexMessage {
+    pub fn new(json: serde_json::Value) -> Self {
+        FlexMessage {
+            type_: "flex".to_string(),
+            altText: "altText:flexMessageです".to_string(),
+            json: json,
         }
     }
 }
-impl Message for FlexMessage{
-    fn json(&self) -> String{
+impl Message for FlexMessage {
+    fn json(&self) -> String {
         serde_json::to_string(self).unwrap()
     }
 }
 
-trait FlexJson:Into<serde_json::Value>{
-}
+trait FlexJson: Into<serde_json::Value> {}
 
-struct VoteFlexJson{
-    json:serde_json::Value
+struct VoteFlexJson {
+    json: serde_json::Value,
 }
-impl VoteFlexJson{
-    fn read_from_file(path:&str)->Self{
-        VoteFlexJson{
-            json:serde_json::from_reader(File::open(path).unwrap()).unwrap()
-        }
-    }
+impl VoteFlexJson {
+    // use std::fs::File;
+    // fn read_from_file(path: &str) -> Self {
+    //     VoteFlexJson {
+    //         json: serde_json::from_reader(File::open(path).unwrap()).unwrap(),
+    //     }
+    // }
 }
-impl Into<serde_json::Value> for VoteFlexJson{
-    fn into(self) -> serde_json::Value{
+impl Into<serde_json::Value> for VoteFlexJson {
+    fn into(self) -> serde_json::Value {
         self.json
     }
 }
-impl FlexJson for VoteFlexJson{
-
-}
+impl FlexJson for VoteFlexJson {}
